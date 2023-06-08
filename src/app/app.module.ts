bd5eb07f08588c9c861dd035c1ff5691c985b330
Daniela <daniela.scholz93@gmail.com>
2023-06-08 20:18:12 +0200

    added following components: about me, skills, portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderMenuComponent } from './header-menu/header-menu.component';
 import { IntroComponent } from './intro/intro.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderMenuComponent,
-    IntroComponent
+    IntroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,

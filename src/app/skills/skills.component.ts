c45d90370f80b600e13f9b2e6217ece08cb9c45d
Daniela <daniela.scholz93@gmail.com>
2023-06-15 19:27:21 +0200

    some bug fixing

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 skills = ['Angular', 'Typescript', 'JavaScript', 'HTML', 'CSS', 'Firebase', 'Git', 'Scrum', 'Rest-Api', 'Material Design'];
+arrow = [1 , 2, 3];
+
+// animateArrow(){
+
+
+// }
+
+// animateArrow() {
+
+
+//   const arrowImage = document.querySelector('.animated-arrow');
+//   arrowImage.classList.add('hover');
+//   arrowImage.addEventListener('transitionend', () => {
+//     arrowImage.setAttribute('src', '../../assets/img/arrow/arrow-left/3.svg');
+//     arrowImage.classList.remove('hover');
+//   }, { once: true });
+
+// }
 
 }
